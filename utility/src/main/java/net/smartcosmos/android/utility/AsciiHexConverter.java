32cef853ea45c1f700b7838254c805cd5ecbf4f6
Jan Bauer <jan.bauer@smartrac-group.com>
2015-12-08 10:53:45 +0100

    Added byte[] AsciiHexConverter.hexToBytesReverse(String)  - to keep the converter in sync with SMART COSMOS Profiles

--- utility/src/main/java/net/smartcosmos/android/utility/AsciiHexConverter.java
+++ utility/src/main/java/net/smartcosmos/android/utility/AsciiHexConverter.java
@@ -48,13 +48,25 @@ public class AsciiHexConverter {
 	    return new String(hexChars);
 	}	
 	
-	public static byte[] hexToBytes(String s) {
-	    int len = s.length();
-	    byte[] data = new byte[len / 2];
-	    for (int i = 0; i < len; i += 2) {
-	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-	                             + Character.digit(s.charAt(i+1), 16));
-	    }
-	    return data;
-	}	
+	public static byte[] hexToBytes(String s)
+	{
+		int len = s.length();
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2)
+		{
+			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+					+ Character.digit(s.charAt(i + 1), 16));
+		}
+		return data;
+	}
+
+    public static byte[] hexToBytesReverse(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[(len - i - 1) / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
 }
